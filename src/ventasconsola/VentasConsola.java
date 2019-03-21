cd25b31461e3de72ed8fa022718dd504a9ead8cb
Daniel <danielc1015@gmail.com>
2019-03-21 11:47:58 -0300

    Project inicialization

---
+++ src/ventasconsola/VentasConsola.java
@@ -0,0 +1,17 @@
+
+package ventasconsola;
+
+/**
+ *
+ * @author danielcandiapereira
+ */
+public class VentasConsola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
